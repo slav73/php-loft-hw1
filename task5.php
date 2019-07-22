784081e32a18dee983ab63e6a4e72a6b376c2426
Slav <shutrow@rambler.ru>
2019-07-23 00:51:10 +0300

    HW1 mistakes corrected

--- task5.php
+++ task5.php
@@ -1,13 +1,17 @@
 <?php
-$bmw = array('X5', 120, 5, '2015');
-$toyota = array('Camry', 100, 5, '2017');
-$opel = array('Cadet', 90, 3, '2001');
+$bmw = ['X5', 120, 5, '2015'];
+$toyota = ['Camry', 100, 5, '2017'];
+$opel = ['Cadet', 90, 3, '2001'];
 
 $cars['bmw'] = $bmw;
 $cars['toyota'] = $toyota;
 $cars['opel'] = $opel;
 
 foreach($cars as $key => $value) {
-    echo "CAR ", $key,
-    "<br />", implode(' ', $value), "<br />";
+    echo "CAR ", $key, PHP_EOL;
+
+    foreach($value as $key1 => $value1) {
+        echo $value1, " ";
+    }
+    echo PHP_EOL;
 }
--- task6.php
+++ task6.php
@@ -4,12 +4,13 @@ echo '<table width="500px" border="1px" align="center">';
 for ($i = 1; $i <= 10; $i++) {
     echo '<tr>';
     for ($j = 1; $j <= 10; $j++) {
+        echo '<td align="center">';
         if ($i % 2 == 1 && $j % 2 == 1) {
-            echo '<td align="center">[', $i * $j, ']</td>';
+            echo '[', $i * $j, ']</td>';
         } elseif ($i % 2 == 0 && $j % 2 == 0) {
-            echo '<td align="center">(', $i * $j, ')</td>';
+            echo '(', $i * $j, ')</td>';
         } else {
-            echo '<td align="center">', $i * $j, '</td>';
+            echo $i * $j, '</td>';
         }
     }    
     echo '</tr>';
